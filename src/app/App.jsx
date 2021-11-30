1f83100555f41fc637bbfc685c89a9fe665cdd49
Viktor Sinelnykov <Fenya.ua@gmail.com>
2021-11-30 09:09:16 +0200

    add markup task first

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import Profile from 'social-profile/social-profile';
+import user from '../data/user.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 }
-
-export default App;
